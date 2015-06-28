3a972ed0da90c308f28651f3d2010895429724e8
syzer3@gmail.com <syzer3@gmail.com>
2015-06-28 10:49:28 +0200

    formatting

--- js/components/composite.jsx
+++ js/components/composite.jsx
@@ -1,50 +1,54 @@
 define([
-  'react',
-  'react-bootstrap/Row',
-  './panel',
-  './logo',
-  './pie',
-  './timeline',
-  './timeline2',
-  './dial',
-  './drawing',
-  './tree'
-], function(React, Row, Panel, Logo, Pie, Timeline, Timeline2, Dial, Drawing, Tree) {
-  return React.createClass({
-    render: function() {
-      return (
-        <div className="container">
-          <Row>
-            <Logo />
-          </Row>
-          <Row>
-            <Panel title="Pie Chart" text="Here is a pie chart example. Sectors are clickable" sources={ ['components/pie', 'charts/pie'] }>
-              <Pie />
-            </Panel>
-            <Panel title="Path animation"
-              text={ <span>Integrate with <a href="http://maxwellito.github.io/vivus/" target="_blank">Vivus</a> to animate paths</span> }
-              sources={ ['components/drawing'] }>
-              <Drawing />
-            </Panel>
-          </Row>
-          <Row>
-            <Panel title="Line Chart" text="Here is a zoomable timeline" sources={ ['components/timeline', 'charts/timeline'] }>
-              <Timeline />
-            </Panel>
-            <Panel title="Zoomable timeline" text="Use the minimap to zoom the timeline"
-              sources={ ['components/timeline2', 'charts/zoomable', 'charts/zoomedmap', 'charts/minimap'] }>
-              <Timeline2 />
-            </Panel>
-          </Row>
-          <Row>
-            <Panel title="Speed dial" text="We use a pie chart to track the mouse speed" sources={ ['components/dial'] }>
-              <Dial />
-            </Panel>
-            <Panel title="Tree" text="Here is a Tree Graph" sources={ ['components/tree'] }>
-              <Tree />
-            </Panel>
-          </Row>
-        </div>
-    )}
-  });
+    'react',
+    'react-bootstrap/Row',
+    './panel',
+    './logo',
+    './pie',
+    './timeline',
+    './timeline2',
+    './dial',
+    './drawing',
+    './tree'
+], function (React, Row, Panel, Logo, Pie, Timeline, Timeline2, Dial, Drawing, Tree) {
+    return React.createClass({
+        render: function () {
+            return (
+                <div className="container">
+                    <Row>
+                        <Logo />
+                    </Row>
+                    <Row>
+                        <Panel title="Pie Chart" text="Here is a pie chart example. Sectors are clickable"
+                               sources={ ['components/pie', 'charts/pie'] }>
+                            <Pie />
+                        </Panel>
+                        <Panel title="Path animation"
+                               text={ <span>Integrate with <a href="http://maxwellito.github.io/vivus/" target="_blank">Vivus</a> to animate paths</span> }
+                               sources={ ['components/drawing'] }>
+                            <Drawing />
+                        </Panel>
+                    </Row>
+                    <Row>
+                        <Panel title="Line Chart" text="Here is a zoomable timeline"
+                               sources={ ['components/timeline', 'charts/timeline'] }>
+                            <Timeline />
+                        </Panel>
+                        <Panel title="Zoomable timeline" text="Use the minimap to zoom the timeline"
+                               sources={ ['components/timeline2', 'charts/zoomable', 'charts/zoomedmap', 'charts/minimap'] }>
+                            <Timeline2 />
+                        </Panel>
+                    </Row>
+                    <Row>
+                        <Panel title="Speed dial" text="We use a pie chart to track the mouse speed"
+                               sources={ ['components/dial'] }>
+                            <Dial />
+                        </Panel>
+                        <Panel title="Tree" text="Here is a Tree Graph" sources={ ['components/tree'] }>
+                            <Tree />
+                        </Panel>
+                    </Row>
+                </div>
+            )
+        }
+    });
 });
--- js/components/pie.jsx
+++ js/components/pie.jsx
@@ -1,31 +1,33 @@
 define([
-  'react',
-  'palette/colors',
-  'charts/pie',
-], function(React, Colors, Pie) {
-  var countries = [
-    { name: 'Italy', population: 59859996 },
-    { name: 'Mexico', population: 118395054 },
-    { name: 'France', population: 65806000 },
-    { name: 'Argentina', population: 40117096 },
-    { name: 'Japan', population: 127290000 }
-  ];
+    'react',
+    'palette/colors',
+    'charts/pie',
+], function (React, Colors, Pie) {
+    var countries = [
+        {name: 'Italy', population: 59859996},
+        {name: 'Mexico', population: 118395054},
+        {name: 'France', population: 65806000},
+        {name: 'Argentina', population: 40117096},
+        {name: 'Japan', population: 127290000}
+    ];
 
-  var palette = Colors.mix({
-    r: 130,
-    g: 140,
-    b: 210
-  }, {
-    r: 180,
-    g: 205,
-    b: 150
-  });
+    var palette = Colors.mix({
+        r: 130,
+        g: 140,
+        b: 210
+    }, {
+        r: 180,
+        g: 205,
+        b: 150
+    });
 
-  function population(c) { return c.population; }
-
-  return React.createClass({
-    render: function() {
-      return <Pie data={ countries } palette={ palette } r={ 60 } R={ 140 } accessor={ population } />
+    function population(c) {
+        return c.population;
     }
-  });
+
+    return React.createClass({
+        render: function () {
+            return <Pie data={ countries } palette={ palette } r={ 60 } R={ 140 } accessor={ population }/>
+        }
+    });
 });
