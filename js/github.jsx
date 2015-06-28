b81236d9a180bd0c7c106b5c8e7716aad3a42d9f
syzer3@gmail.com <syzer3@gmail.com>
2015-06-28 10:51:42 +0200

    formatting

--- js/github.jsx
+++ js/github.jsx
@@ -1,12 +1,14 @@
 define([
-  'pajamas'
-], function(pj) {
-  var base_url = 'https://api.github.com/repos/andreaferretti/paths-js-react-demo/contents/js/';
+    'pajamas'
+], function (pj) {
+    var base_url = 'https://api.github.com/repos/andreaferretti/paths-js-react-demo/contents/js/';
 
-  return function(path) {
-    return pj({
-      url: base_url + path + '.jsx',
-      dataType :'json'
-    }).then(function(data) { return atob(data.content); });
-  };
+    return function (path) {
+        return pj({
+            url: base_url + path + '.jsx',
+            dataType: 'json'
+        }).then(function (data) {
+            return atob(data.content);
+        });
+    };
 });
--- js/main.jsx
+++ js/main.jsx
@@ -1,23 +1,23 @@
 requirejs.config({
-  baseUrl: './js',
-  paths: {
-    'text' :               '../../bower_components/requirejs-text/text',
-    'json' :               '../../bower_components/requirejs-plugins/src/json',
-    'react':               '../../bower_components/react/react-with-addons',
-    'react-bootstrap':     '../../bower_components/react-bootstrap',
-    'q':                   '../../bower_components/q/q',
-    'pajamas':             '../../bower_components/pajamas/dist/pajamas',
-    'lodash':              '../../bower_components/lodash/lodash',
-    'paths':               '../../bower_components/paths-js/dist/amd',
-    'vivus':               '../../bower_components/vivus/dist/vivus',
-    'hljs':                '../../highlight/highlight.pack',
-    'data':                '../data'
-  },
-  shim: {
-    'vivus': { exports: 'Vivus' }
-  }
+    baseUrl: './js',
+    paths: {
+        'text': '../../bower_components/requirejs-text/text',
+        'json': '../../bower_components/requirejs-plugins/src/json',
+        'react': '../../bower_components/react/react-with-addons',
+        'react-bootstrap': '../../bower_components/react-bootstrap',
+        'q': '../../bower_components/q/q',
+        'pajamas': '../../bower_components/pajamas/dist/pajamas',
+        'lodash': '../../bower_components/lodash/lodash',
+        'paths': '../../bower_components/paths-js/dist/amd',
+        'vivus': '../../bower_components/vivus/dist/vivus',
+        'hljs': '../../highlight/highlight.pack',
+        'data': '../data'
+    },
+    shim: {
+        'vivus': {exports: 'Vivus'}
+    }
 });
 
-require(['react', 'components/composite'], function(React, Composite) {
-  return React.render(<Composite />, document.body);
+require(['react', 'components/composite'], function (React, Composite) {
+    return React.render(<Composite />, document.body);
 });
