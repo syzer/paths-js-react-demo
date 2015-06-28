b0e0aef30b5946761cbf09127664507a6d98abf9
syzer3@gmail.com <syzer3@gmail.com>
2015-06-28 10:47:25 +0200

    formatting

--- js/charts/pie.jsx
+++ js/charts/pie.jsx
@@ -1,82 +1,99 @@
 define([
-  'react',
-  'animate',
-  'palette/colors',
-  'paths/pie',
-], function(React, Animate, Colors, Pie) {
+    'react',
+    'animate',
+    'palette/colors',
+    'paths/pie',
+], function (React, Animate, Colors, Pie) {
 
-  function cyclic(coll, i) { return coll[i % coll.length]; }
-  function identity(x) { return x }
+    function cyclic(coll, i) {
+        return coll[i % coll.length];
+    }
 
-  return React.createClass({
-    mixins: [Animate.Mixin],
+    function identity(x) {
+        return x
+    }
 
-    getInitialState: function() {
-      return { expanded: [0, 0, 0, 0, 0] };
-    },
+    return React.createClass({
+        mixins: [Animate.Mixin],
 
-    translate: function(p) { return "translate(" + p[0] + "," + p[1] + ")" },
+        getInitialState: function () {
+            return {expanded: [0, 0, 0, 0, 0]};
+        },
 
-    move: function(point, perc) {
-      return this.translate([point[0] * perc / 3, point[1] * perc / 3]);
-    },
+        translate: function (p) {
+            return "translate(" + p[0] + "," + p[1] + ")"
+        },
 
-    grad: function(i) { return "grad-" + i },
+        move: function (point, perc) {
+            return this.translate([point[0] * perc / 3, point[1] * perc / 3]);
+        },
 
-    fill: function(i) { return "url(#grad-" + i  +")" },
+        grad: function (i) {
+            return "grad-" + i
+        },
 
-    color: function(i) { return Colors.string(cyclic(this.props.palette, i)); },
+        fill: function (i) {
+            return "url(#grad-" + i + ")"
+        },
 
-    lighten: function(i) { return Colors.string(Colors.lighten(cyclic(this.props.palette, i))); },
+        color: function (i) {
+            return Colors.string(cyclic(this.props.palette, i));
+        },
 
-    expand: function(i) {
-      var self = this;
-      return function() {
-        var target = [0, 0, 0, 0, 0];
-        target[i] = 1;
-        self.animateState({ expanded: target });
-      };
-    },
+        lighten: function (i) {
+            return Colors.string(Colors.lighten(cyclic(this.props.palette, i)));
+        },
 
-    render: function() {
-      var chart = Pie({
-        center: this.props.center || [0,0],
-        r: this.props.r || 60,
-        R: this.props.R || 140,
-        data: this.props.data,
-        accessor: this.props.accessor || identity
-      });
-      var self = this;
-      var coefficients = this.state.expanded;
-      var slices = chart.curves.map(function(c, i) {
-        return (
-          <g key={ i } transform={ self.move(c.sector.centroid, coefficients[i]) }>
-            <linearGradient id={ self.grad(i) }>
-              <stop stopColor={ self.color(i) } offset="0%"/>
-              <stop stopColor={ self.lighten(i) } offset="100%"/>
-            </linearGradient>
-            <path onClick={ self.expand(i) } d={ c.sector.path.print() } fill={ self.fill(i) } />
-            <text textAnchor="middle" transform={ self.translate(c.sector.centroid) }>{ c.item.name }</text>
-          </g>
-        )
-      });
-      var selected = _.find(this.props.data, function(c, i) {
-        return coefficients[i] === 1;
-      });
-      var table = selected ?
-        <div className="country-info">
-          <h4>{ selected.name }</h4>
-          <p>Population: <span className="label label-info">{ selected.population }</span></p>
-        </div> : null
+        expand: function (i) {
+            var self = this;
+            return function () {
+                var target = [0, 0, 0, 0, 0];
+                target[i] = 1;
+                self.animateState({expanded: target});
+            };
+        },
 
-      return(
-        <div id="pie">
-          <svg width="375" height="400">
-            <g transform="translate(200, 200)">{ slices }</g>
-          </svg>
+        render: function () {
+            var chart = Pie({
+                center: this.props.center || [0, 0],
+                r: this.props.r || 60,
+                R: this.props.R || 140,
+                data: this.props.data,
+                accessor: this.props.accessor || identity
+            });
+            var self = this;
+            var coefficients = this.state.expanded;
+            var slices = chart.curves.map(function (c, i) {
+                return (
+                    <g key={ i } transform={ self.move(c.sector.centroid, coefficients[i]) }>
+                        <linearGradient id={ self.grad(i) }>
+                            <stop stopColor={ self.color(i) } offset="0%"/>
+                            <stop stopColor={ self.lighten(i) } offset="100%"/>
+                        </linearGradient>
+                        <path onClick={ self.expand(i) } d={ c.sector.path.print() } fill={ self.fill(i) }/>
+                        <text textAnchor="middle" transform={ self.translate(c.sector.centroid) }>{ c.item.name }</text>
+                    </g>
+                )
+            });
+            var selected = _.find(this.props.data, function (c, i) {
+                return coefficients[i] === 1;
+            });
+            var table = selected ?
+                <div className="country-info">
+                    <h4>{ selected.name }</h4>
 
-          { table }
-        </div>
-    )}
-  });
+                    <p>Population: <span className="label label-info">{ selected.population }</span></p>
+                </div> : null
+
+            return (
+                <div id="pie">
+                    <svg width="375" height="400">
+                        <g transform="translate(200, 200)">{ slices }</g>
+                    </svg>
+
+                    { table }
+                </div>
+            )
+        }
+    });
 });
